f58f1bf7ed7a9a0c0267e814a5274b13c287df6d
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-09-18 10:19:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,3 +8,13 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
   ]
+  
+    # Helper Method
+  
+  def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
