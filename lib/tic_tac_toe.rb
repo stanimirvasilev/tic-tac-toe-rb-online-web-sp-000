88ca42aec8f67dc6d69f70cdf1a8f26c4c7d961d
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-09-18 10:04:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,4 +9,37 @@ WIN_COMBINATIONS = [
   [2,4,6]
   ]
   
-  
+  # Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+  board[win_combination[0]] == board[win_combination[1]] &&
+  board[win_combination[1]] == board[win_combination[2]] &&
+  position_taken?(board, win_combination[0])
+  end
+end 
+
+
+ def full?(board)
+    board.all? {|full_board| full_board == "X" || full_board== "O" }
+ end
+ 
+ def draw?(board)
+   full?(board) && !won?(board)
+ end
+ 
+ def over?(board) 
+  draw?(board) || won?(board)
+ end
+ 
+ def winner(board) 
+   #binding.pry
+   win_combination = won?(board)
+   if won?(board) 
+    board[win_combination[0]]
+   end
+ end 
+
