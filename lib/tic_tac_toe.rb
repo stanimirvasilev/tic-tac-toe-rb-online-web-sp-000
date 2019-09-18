e438c45b737f33969da1e987a5dae955a1d1f846
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-09-18 10:26:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ end
 
 def valid_move?(board, index) 
   if position_taken?(board, index) && index.between?(0, 8) 
-  if position_taken?(board, index)  == true && index.between?(0, 8) == false
+  elsif position_taken?(board, index)  == true && index.between?(0, 8) == false
     return false
   elsif position_taken?(board, index)  == false && index.between?(0, 8) == true
   return true
