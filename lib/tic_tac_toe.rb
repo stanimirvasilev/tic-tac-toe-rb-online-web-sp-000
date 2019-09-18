3da486d926a5467ed7175fb9ac86a37fa57e904d
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-09-18 10:24:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,3 +27,11 @@ def move(board, index, player)
 board[index] = player 
 end
 
+def position_taken? (board, index)
+  if board[index] == " " || board[index]  == "" || board[index]  == nil
+    return false
+  elsif board[index]  == "X" || board[index]  == "O"
+    return true
+  else 
+end
+  end
