aceeaaf07ff50385d85f4a4f6646a78818c09e22
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-09-18 10:07:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,7 +19,15 @@ WIN_COMBINATIONS = [
   end
 end 
   
-  
+  def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      counter += 1 
+     end
+   end
+   counter
+end
   
   
 def position_taken?(board, index)
